ae235006b478ea745dc9eeadb6b757911466571d
lunarxlark <lunarxlark@gmail.com>
2023-03-31 18:20:17 +0900

    feat(chat):add `continue`

--- cmd/chat/continue.go
+++ cmd/chat/continue.go
@@ -4,40 +4,73 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
-	"time"
+	"strings"
 
-	"github.com/lunarxlark/oai-go/cmd/model"
+	ff "github.com/ktr0731/go-fuzzyfinder"
 	"github.com/urfave/cli/v2"
 )
 
 func CmdContinue(ctx *cli.Context) error {
-	model, err := model.List()
+	fs, err := ListLog()
 	if err != nil {
 		return err
 	}
 
-	log, err := os.Create(filepath.Join(os.Getenv("XDG_CONFIG_HOME"), "oai", "chat", fmt.Sprintf("%d.json", time.Now().Unix())))
+	i, err := ff.Find(fs, func(i int) string {
+		return fs[i]
+	},
+		ff.WithPreviewWindow(func(i, w, h int) string {
+			fb, err := os.ReadFile(filepath.Join(os.Getenv("XDG_CONFIG_HOME"), "oai", "chat", fs[i]))
+			if err != nil {
+				log.Fatal(fmt.Errorf("failed to read log files for preview. %w", err))
+			}
+			log.Println(string(fb))
+
+			conversation := new(Conversation)
+			if err := json.Unmarshal(fb, &conversation); err != nil {
+				log.Fatal(fmt.Errorf("failed to unmarshal json for log preview. %w", err))
+			}
+			var preview strings.Builder
+			for _, m := range conversation.Messages {
+				switch m.Role {
+				case user:
+					preview.WriteString(fmt.Sprintf("> %s\n", m.Content))
+				default:
+					preview.WriteString(m.Content + "\n")
+				}
+			}
+			return fmt.Sprintf("Summary : %s\nModel : %s\nMessage : \n%s", conversation.Summary, conversation.Model, preview.String())
+		}))
+	if err != nil {
+		return fmt.Errorf("failed to fuzzy-find for log file. %w", err)
+	}
+
+	fb, err := os.ReadFile(filepath.Join(os.Getenv("XDG_CONFIG_HOME"), "oai", "chat", fs[i]))
 	if err != nil {
 		return err
 	}
-	defer log.Close()
 
 	conversation := new(Conversation)
+	if err := json.Unmarshal(fb, &conversation); err != nil {
+		return err
+	}
 
 	sc := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Printf("%s > ", user)
 		if !sc.Scan() {
-			conversation.Summary, err = CreateSummay(model, conversation.Messages)
+			conversation.Summary, err = CreateSummay(conversation.Model, conversation.Messages)
 			b, err := json.MarshalIndent(conversation, "", "  ")
 			if err != nil {
 				return err
 			}
-			if _, err := log.Write(b); err != nil {
+			if err := os.WriteFile(filepath.Join(os.Getenv("XDG_CONFIG_HOME"), "oai", "chat", fs[i]), b, 0644); err != nil {
 				return err
 			}
+			break
 		}
 		statement := []Message{{
 			Role:    user,
@@ -45,7 +78,7 @@ func CmdContinue(ctx *cli.Context) error {
 		}}
 		conversation.Messages = append(conversation.Messages, statement...)
 
-		res, err := CreateReq(model, statement).Request()
+		res, err := CreateReq(conversation.Model, conversation.Messages).Request()
 		if err != nil {
 			return err
 		}
@@ -55,4 +88,20 @@ func CmdContinue(ctx *cli.Context) error {
 			conversation.Messages = append(conversation.Messages, choice.Message)
 		}
 	}
+	return nil
+}
+
+func ListLog() ([]string, error) {
+	des, err := os.ReadDir(filepath.Join(os.Getenv("XDG_CONFIG_HOME"), "oai", "chat"))
+	if err != nil {
+		return nil, err
+	}
+
+	fnames := []string{}
+	for _, de := range des {
+		if !de.IsDir() {
+			fnames = append(fnames, de.Name())
+		}
+	}
+	return fnames, nil
 }
