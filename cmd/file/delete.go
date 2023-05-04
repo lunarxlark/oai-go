aa7d1192a88b343ba57a9859395cfc481dfff45c
lunarxlark <lunarxlark@gmail.com>
2023-05-05 00:02:54 +0900

    feat(cmd):add file delete command (#18)

---
+++ cmd/file/delete.go
@@ -0,0 +1,32 @@
+package file
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/lunarxlark/openai-cli/api"
+	"github.com/urfave/cli/v2"
+)
+
+type ResponseFileDelete struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Deleted bool   `json:"deleted"`
+}
+
+func Delete(ctx *cli.Context) error {
+
+	url := fmt.Sprintf("%s/%s", url, ctx.String("file_id"))
+
+	var res ResponseFileDelete
+	if err := api.Request(http.MethodDelete, url, nil, &res); err != nil {
+		return err
+	}
+
+	if res.Deleted {
+		fmt.Println("success file delete")
+		fmt.Printf("ID:%s", res.ID)
+	}
+
+	return nil
+}
